e8366f895f6ed00b2c20dcb3a62ff1dd8aaea003
Andrii Trofimchuk <trofimchuk.an@gmail.com>
2020-04-09 21:05:09 +0300

    Refactor search

--- app/pages/cars_Info.py
+++ app/pages/cars_Info.py
@@ -1,16 +1,24 @@
 vendor_list = [
     "Audi",
     "BMW",
-    "Volvo"
+    "Volvo",
+    "Ford"
 ]
 
 models_list = [
     "A6",
     "X3",
-    "CX60"
+    "CX60",
+    "Mustang GT"
 ]
 
 engine_list = [
     "Diesel",
     "Benzin"
+]
+
+transmission_list = [
+    "Automatic",
+    "Manual",
+    "Robot"
 ]
--- app/pages/views.py
+++ app/pages/views.py
@@ -2,14 +2,42 @@ from django.shortcuts import render
 
 from cars.models import CarsList
 from carmanager.models import CarManager
-from .cars_Info import vendor_list, models_list, engine_list
+from .cars_Info import vendor_list, models_list, engine_list, transmission_list
 
 
 def index(request):
-
     cars = CarsList.objects.all().filter(is_published=True)[:4]
+    query = CarsList.objects.order_by("vendor")
+
+    if "vendor" in request.GET:
+        vendor = request.GET["vendor"]
+        if vendor:
+            query = query.filter(vendor__iexact=vendor)
+
+    if "model" in request.GET:
+        model = request.GET["model"]
+        if model:
+            query = query.filter(model__iexact=model)
+
+    if "engine" in request.GET:
+        engine = request.GET["engine"]
+        if engine:
+            query = query.filter(engine__iexact=engine)
+
+    if "transmission" in request.GET:
+        transmission = request.GET["transmission"]
+        if transmission:
+            query = query.filter(transmission__iexact=transmission)
+
     context = {
-        'cars': cars
+        'cars': cars,
+        "vendor_list": vendor_list,
+        "models_list": models_list,
+        "engine_list": engine_list,
+        "transmission_list": transmission_list,
+        "search_cars": query,
+        "request_value": request.GET
+
     }
 
     return render(request, 'pages/index.html', context)
@@ -54,11 +82,18 @@ def search(request):
         if engine:
             query = query.filter(engine__iexact=engine)
 
+    if "transmission" in request.GET:
+        transmission = request.GET["transmission"]
+        if transmission:
+            query = query.filter(transmission__iexact=transmission)
+
     context = {
         "vendor_list": vendor_list,
         "models_list": models_list,
         "engine_list": engine_list,
+        "transmission_list": transmission_list,
         "cars": query,
+        "request_value": request.GET
     }
 
     return render(request, 'pages/search.html', context)
