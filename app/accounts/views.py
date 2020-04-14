eff3b38fef265b5648692236b050045cc2619d05
Andrii Trofimchuk <trofimchuk.an@gmail.com>
2020-04-14 20:31:21 +0300

    Added login

--- app/accounts/views.py
+++ app/accounts/views.py
@@ -16,19 +16,37 @@ def register(request):
             if User.objects.filter(username=username).exists():
                 messages.error(request, "Username alrady taken.")
                 return redirect(register)
-            elif User.objects.filter(email=email).exists():
-                messages.error(request, "Email alrady taken.")
-                return redirect(register)
+            else:
+                if User.objects.filter(email=email).exists():
+                    messages.error(request, "Email alrady taken.")
+                    return redirect(register)
+                else:
+                    user = User.objects.create_user(
+                        username=username, email=email, password=password, first_name=first_name, last_name=last_name,)
+                    user.save()
+                    messages.success(request, "You are registred. Log in")
+                    return redirect('login')
         else:
             messages.error(request, "Passwords do not match")
-        return render(request, "accounts/register.html")
+        return redirect('register')
     else:
         return render(request, "accounts/register.html")
 
 
 def login(request):
     if request.method == "POST":
-        return
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            messages.success(request, "You are logged in")
+            return redirect('dashboard')
+        else:
+            messages.error(request, "Login or password incorrect")
+            return redirect('login')
     else:
         return render(request, "accounts/login.html")
 
